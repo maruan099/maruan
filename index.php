779641179bf15f2db0017ad3ac128f899f88e60d
Mar1 <Mar1@Maruan.elahia.local>
2017-10-02 17:05:56 +0200

    Creat el metode precio

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        
+        class mecanica {
+            
+    // El constructor
+        public function __construct($pieza,$precio){
+            $this->pieza =$pieza;
+            $this->precio =$precio;
+          }            
+    //Metodes
+          
+        public function getPrecio(){
+    
+    // Retornem el atribut preu
+            return $this->precio;
+        }
+          
+        } 
+        ?>
+    </body>
+</html>
